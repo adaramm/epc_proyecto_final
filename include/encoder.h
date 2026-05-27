ae98c9996de54eacd958310e88e0933d382bea34
Adara Mercado <ariel.mercado@ingenieria.unam.edu>
2026-05-27 16:22:59 -0600

    Delete include directory

--- include/encoder.h
+++
@@ -1,6 +0,0 @@
-#ifndef ENCODER_H
-#define ENCODER_H
-
-void encode_mov_reg_imm(int reg, int imm);
-
-#endif
--- include/linker.h
+++
@@ -1,6 +0,0 @@
-#ifndef LINKER_H
-#define LINKER_H
-
-void linker_run(int count, char **files);
-
-#endif
