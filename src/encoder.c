58e79f5533e75b4d9ba31a0a6a9ff0646c783f53
Adara Mercado <ariel.mercado@ingenieria.unam.edu>
2026-05-27 16:23:10 -0600

    Delete src directory

--- src/encoder.c
+++
@@ -1,9 +0,0 @@
-#include "sib.h"
-
-unsigned char build_sib(
-    unsigned char scale,
-    unsigned char index,
-    unsigned char base
-) {
-    return (scale << 6) | (index << 3) | base;
-}
--- src/modrm.c
+++
@@ -1,9 +0,0 @@
-#include "modrm.h"
-
-unsigned char build_modrm(
-    unsigned char mod,
-    unsigned char reg,
-    unsigned char rm
-) {
-    return (mod << 6) | (reg << 3) | rm;
-}
